7b0a4438ec306837b66013f23a698661b4754859
tytydraco <tylernij@gmail.com>
2020-10-29 21:33:40 -0700

    Improve
    Signed-off-by: tytydraco <tylernij@gmail.com>

--- app/src/main/java/com/draco/emulauncher/MainActivity.kt
+++ app/src/main/java/com/draco/emulauncher/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.draco.emulauncher
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private fun getAppList(): Array<AppInfo> {
-        val a = AppInfo().also { it ->
-            it.image = ContextCompat.getDrawable(this, R.drawable.snes)!!
-            it.label = "Super Nintendo Entertainment System"
-            val intent = packageManager.getLaunchIntentForPackage("com.retroarch.aarch64")!!
-            it.launchIntent = intent
+        val launcherIntent = Intent(Intent.ACTION_MAIN, null)
+        launcherIntent.addCategory(Intent.CATEGORY_LAUNCHER)
+
+        val activities = packageManager.queryIntentActivities(launcherIntent, 0)
+        val appList = ArrayList<AppInfo>()
+
+        for (app in activities) {
+            val appId = app.activityInfo.packageName
+            if (appId == packageName)
+                continue
+
+            AppInfo().apply {
+                image = packageManager.getApplicationIcon(appId)
+                label = app.activityInfo.loadLabel(packageManager).toString()
+                launchIntent = packageManager.getLaunchIntentForPackage(appId)!!
+                appList.add(this)
+            }
         }
-        return arrayOf(a)
+
+        appList.sortBy {
+            it.label.toLowerCase(Locale.getDefault())
+        }
+
+        return appList.toTypedArray()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,11 +43,13 @@ class MainActivity : AppCompatActivity() {
         val recycler = findViewById<RecyclerView>(R.id.recycler)
         val recyclerAdapter = RecyclerAdapter(getAppList(), this)
         val manager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        manager.scrollToPosition(Integer.MAX_VALUE / 2)
 
         with (recycler) {
             adapter = recyclerAdapter
             layoutManager = manager
+            PagerSnapHelper().attachToRecyclerView(this)
+            scrollToPosition(Integer.MAX_VALUE / 2)
+            smoothScrollToPosition(Integer.MAX_VALUE / 2 + 5)
         }
     }
 }
